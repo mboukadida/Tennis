833014117619a1b6a95533e77fb22af51ac84bcf
Majed BOUKADIDA <majed.boukadida@gmail.com>
2017-11-13 17:29:48 +0100

    change coverage path

--- Tennis/Tennis.Tests/coverage.bat
+++
@@ -1,5 +0,0 @@
-.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user "-filter:+[Tennis]* -[*Test]*" "-target:.\packages\NUnit.ConsoleRunner.3.7.0\tools\nunit3-console.exe" "-targetargs:/noshadow bin\Debug\Tennis.Tests.dll"
-
-.\packages\ReportGenerator.3.0.2\tools\ReportGenerator.exe "-reports:results.xml" "-targetdir:.\coverage"
-
-pause
