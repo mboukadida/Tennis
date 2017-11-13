30d1493d19f19629cd0d6be4c3329333f304a5b0
Majed BOUKADIDA <majed.boukadida@cellenza.com>
2017-11-13 19:43:35 +0100

    change

--- Tennis/coverage.bat
+++ Tennis/coverage.bat
@@ -1,4 +1,4 @@
-Tennis\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user "-filter:+[Tennis]* -[*Test]*" "-target:Tennis\packages\NUnit.ConsoleRunner.3.7.0\tools\nunit3-console.exe" "-targetargs:Tennis\Tennis.Tests\bin\Debug\Tennis.Tests.dll"
+Tennis\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user "-filter:+[Tennis]* -[*Test]*" "-target:Tennis\packages\NUnit.ConsoleRunner.3.7.0\tools\nunit3-console.exe" "-targetargs:Tennis\Tennis.Tests\bin\Debug\Tennis.Tests.dll" -output:"Tennis\TestResults\opencover.xml"
 
 Tennis\packages\ReportGenerator.3.0.2\tools\ReportGenerator.exe "-reports:TestResults\results.xml" "-targetdir:.\coverage"
 
